c98a8527f313760ef377ab456f4dfc81ed296ebd
Linda Chang <lindachang186@gmail.com>
2018-11-27 22:15:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,17 +25,8 @@ class Artist
 
   def self.find_or_create_by_name(name)
     self.all.find{|artist| artist.name == name} ? self.all.find{|artist| artist.name == name} : self.new(name).tap {|artist| artist.save}
-    #self.find(name) ? self.find(name) : self.create(name)
+    #self.find(name) ? self.find(name) : self.create(name) You could have done this if you had a find and a create method separately
   end
-
-  # def self.find(name)
-  #   self.all.find {|artist| artist.name == name}
-  # end
-  #
-  # def self.create(name)
-  #   self.new(name).tap {|artist| artist.save}
-  # end
-
 
   def print_songs
     @songs.each do |song|
