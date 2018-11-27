5c2f1177c8f01d2c7cf52780a91d58033908a0e6
Linda Chang <lindachang186@gmail.com>
2018-11-27 21:41:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
 
 
   def self.new_by_filename(filename)
-   song = Song.new(filename.split(" - ")[1])
+   song = self.new(filename.split(" - ")[1])
    artist = filename.split(" - ")[0]
    song.artist_name = artist
 
