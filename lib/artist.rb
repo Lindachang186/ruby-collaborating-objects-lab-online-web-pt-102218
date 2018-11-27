fa174000af9d6bc824a59b887dbd41cf9d612eb1
Linda Chang <lindachang186@gmail.com>
2018-11-27 22:05:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    found_artist = self.all.find{|artist| artist.name == name} ? found_artist : self.new(name)
+    #found_artist = self.all.find{|artist| artist.name == name} ? found_artist : self.new(name)
     self.find(name) ? self.find(name) : self.create(name)
     #if found_artist
     #  found_artist
